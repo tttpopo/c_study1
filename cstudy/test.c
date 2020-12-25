30b07f6395a95207a6f9d0812bb63f7dbf5a65e1
tttpopo <2924136842@qq.com>
2020-12-25 16:10:24 +0800

    zhizhen

--- cstudy/test.c
+++ cstudy/test.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+int aa = 0;
+int *ap = &aa;
 int main()
 {
 
-    printf("hello ");
+    //printf(printf("%p\n", &aa));
+    // printf("%d\n", sizeof(int));
+
+    printf("%d\n", printf("hello whold"));
+
+    printf("%d", sizeof("hello whold"));
+
     return 0;
 }
